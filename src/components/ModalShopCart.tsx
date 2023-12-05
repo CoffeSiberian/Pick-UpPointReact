70ac6ed4bf2daca60d090ab802a2e527186ca58d
Fernando Garrido <htf754@gmail.com>
2023-12-05 18:36:40 +0000

    Add total price and payment button to ModalShopCart component

--- src/components/ModalShopCart.tsx
+++ src/components/ModalShopCart.tsx
@@ -1,5 +1,6 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 import { useShopCart } from "../hooks/ShopCartContex";
+import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import Dialog from "@mui/material/Dialog";
@@ -8,6 +9,7 @@ import DialogTitle from "@mui/material/DialogTitle";
 
 // icons
 import CloseIcon from "@mui/icons-material/Close";
+import PaidIcon from "@mui/icons-material/Paid";
 
 interface ModalShopCartProps {
     open: boolean;
@@ -16,11 +18,14 @@ interface ModalShopCartProps {
 
 const ModalShopCart: FC<ModalShopCartProps> = ({ open, setOpen }) => {
     const { shopCart } = useShopCart();
+    const total = useRef<number>(0);
 
     const renderShopCart = (): JSX.Element => {
         if (shopCart === null) return <></>;
+        total.current = 0;
 
         const render = shopCart.map((item) => {
+            total.current += item.price * item.quantity;
             return (
                 <div className="flex flex-row" key={item.id}>
                     <div className="grid grid-cols-4 w-full">
@@ -45,6 +50,12 @@ const ModalShopCart: FC<ModalShopCartProps> = ({ open, setOpen }) => {
                 </div>
             );
         });
+
+        const totalString = total.current.toLocaleString("es-CL", {
+            style: "currency",
+            currency: "CLP",
+        });
+
         return (
             <div className="flex flex-col w-full">
                 <div className="grid grid-cols-4">
@@ -54,6 +65,18 @@ const ModalShopCart: FC<ModalShopCartProps> = ({ open, setOpen }) => {
                     <Typography variant="h6">Total Producto</Typography>
                 </div>
                 {render}
+                <div className="flex flex-col gap-1 items-center mt-4">
+                    <Typography variant="h6">Total: {totalString}</Typography>
+                    <div>
+                        <Button
+                            color="success"
+                            variant="outlined"
+                            startIcon={<PaidIcon />}
+                        >
+                            Pagar
+                        </Button>
+                    </div>
+                </div>
             </div>
         );
     };
