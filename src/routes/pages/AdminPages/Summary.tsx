1d656337f6e01c430360d023468a95c4445f5505
Fernando Garrido <htf754@gmail.com>
2023-12-19 17:32:13 +0000

    Add PieChart component to Summary page

--- src/routes/pages/AdminPages/Summary.tsx
+++ src/routes/pages/AdminPages/Summary.tsx
@@ -1,3 +1,4 @@
+import { PieChart } from "@mui/x-charts/PieChart";
 import { useUser } from "../../../hooks/UserContex";
 import { useDarkMode } from "../../../hooks/DarkModeContex";
 import { Typography } from "@mui/material";
@@ -6,22 +7,46 @@ const Summary = () => {
     const { UserInfo } = useUser();
     const { themeTatailwind } = useDarkMode();
 
+    const chartData = () => {
+        return (
+            <PieChart
+                series={[
+                    {
+                        data: [
+                            { id: 0, value: 10, label: "Pendiente" },
+                            { id: 1, value: 15, label: "Pagado" },
+                            { id: 2, value: 3, label: "Rechazado" },
+                            { id: 3, value: 7, label: "Anulado" },
+                        ],
+                    },
+                ]}
+                width={600}
+                height={300}
+            />
+        );
+    };
+
     return (
-        <div>
-            <Typography
-                color={themeTatailwind.primary.color}
-                variant="h4"
-                className="text-center"
-            >
-                Resumen
-            </Typography>
-            <Typography
-                color={themeTatailwind.primary.color}
-                variant="h6"
-                className="text-center"
-            >
-                Bienvenido <b>{UserInfo ? UserInfo.username : "Cargando..."}</b>
-            </Typography>
+        <div className="flex justify-center w-full">
+            <div className="flex flex-col gap-3 max-w-2xl">
+                <Typography
+                    color={themeTatailwind.primary.color}
+                    variant="h4"
+                    className="text-center"
+                >
+                    Resumen
+                </Typography>
+                <Typography
+                    color={themeTatailwind.primary.color}
+                    variant="h6"
+                    className="text-center"
+                >
+                    Bienvenido{" "}
+                    <b>{UserInfo ? UserInfo.username : "Cargando..."}</b>
+                </Typography>
+
+                {chartData()}
+            </div>
         </div>
     );
 };
