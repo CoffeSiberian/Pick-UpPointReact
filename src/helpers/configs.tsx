e6ce22acfe55e5b0a92a64c27ef6a8385fd9b131
Fernando Garrido <htf754@gmail.com>
2024-02-11 20:34:09 +0000

    BREAKING CHANGES CRA was deprecated and new compiler (VITE) was added

---
+++ src/helpers/configs.tsx
@@ -0,0 +1,4 @@
+export const TITLE: string = import.meta.env.VITE_TITLE_SITE!;
+export const API_URL: string = import.meta.env.VITE_REST_API_URL!;
+export const CAPTCHA_KEY: string = import.meta.env.VITE_CAPTCHA_KEY!;
+export const FK_STORE: string = import.meta.env.VITE_FK_STORE!;
--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import App from "./App";
 import { DarkMode } from "./hooks/DarkModeContex";
 import { UserInfo } from "./hooks/UserContex";
 import { ShopCartInfo } from "./hooks/ShopCartContex";
-import reportWebVitals from "./reportWebVitals";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -27,8 +26,3 @@ root.render(
         </UserInfo>
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
