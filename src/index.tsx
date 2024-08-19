c5113c587c548d79aad4eb3681d2c240a1f29571
Fernando Garrido <htf754@gmail.com>
2024-08-19 20:38:28 +0000

    chore: Unnecessary dependencies deleted

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+// components
 import App from "./App";
+
+// hooks
 import { DarkMode } from "./hooks/DarkModeContex";
 import { UserInfo } from "./hooks/UserContex";
 import { ShopCartInfo } from "./hooks/ShopCartContex";
 
+// styles
+import "./index.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
-);
-
-root.render(
-    <React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
         <UserInfo>
             <DarkMode>
                 <ShopCartInfo>
@@ -24,5 +25,5 @@ root.render(
                 </ShopCartInfo>
             </DarkMode>
         </UserInfo>
-    </React.StrictMode>
+    </StrictMode>
 );
