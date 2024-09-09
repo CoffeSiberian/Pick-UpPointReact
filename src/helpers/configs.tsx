ddf07d45ec2ffe5d2a5c145e9dda42930f6635b8
Fernando Garrido <htf754@gmail.com>
2024-09-09 13:44:14 +0000

    hide unnecessary eslint warning in environment variables

--- src/helpers/configs.tsx
+++ src/helpers/configs.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 export const TITLE: string = import.meta.env.VITE_TITLE_SITE!;
 export const API_URL: string = import.meta.env.VITE_REST_API_URL!;
 export const CAPTCHA_KEY: string = import.meta.env.VITE_CAPTCHA_KEY!;
