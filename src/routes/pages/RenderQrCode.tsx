85322e24caad28d1b8b6bb936d582224f706b83f
Fernando Garrido <htf754@gmail.com>
2024-11-30 23:47:01 +0000

    Replace react-qr-code with qrcode.react for QR code generation and implement download functionality

--- src/routes/pages/RenderQrCode.tsx
+++ src/routes/pages/RenderQrCode.tsx
@@ -1,65 +1,80 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
+
+// Context and hooks
 import { useParams } from "react-router-dom";
 import { DarkModeContex } from "../../hooks/DarkModeContex";
+
+// MUI
 import Typography from "@mui/material/Typography";
-import QRCode from "react-qr-code";
 import Button from "@mui/material/Button";
+
+// components
+import { QRCodeCanvas } from "qrcode.react";
+
+// icons
+import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
 
 const RenderQrCode = () => {
 	const { id } = useParams();
 	const { themeTatailwind } = useContext(DarkModeContex);
+	const qrRef = useRef<HTMLDivElement | null>(null);
 
-	const onImageCownload = () => {
-		const fileName = id ? id : "QRCode";
-		const svg = document.getElementById("QRCode");
-		if (svg === null) return;
+	const downloadQRCode = (fileName: string) => {
+		if (!qrRef.current) return;
 
-		const svgData = new XMLSerializer().serializeToString(svg);
-		const canvas = document.createElement("canvas");
+		const canvas = qrRef.current.querySelector("canvas");
+		if (canvas) {
+			const dataURL = canvas.toDataURL("image/png");
+			const link = document.createElement("a");
 
-		const ctx = canvas.getContext("2d");
-		if (ctx === null) return;
-
-		const img = new Image();
-		img.onload = () => {
-			canvas.width = img.width;
-			canvas.height = img.height;
-			ctx.drawImage(img, 0, 0);
-			const pngFile = canvas.toDataURL("image/png");
-			const downloadLink = document.createElement("a");
-			downloadLink.download = fileName;
-			downloadLink.href = `${pngFile}`;
-			downloadLink.click();
-		};
-
-		img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
-	};
-
-	const renderQr = (): JSX.Element => {
-		if (!id) return <></>;
-
-		return (
-			<>
-				<QRCode id="QRCode" value={id} />
-				<Button variant="contained" onClick={onImageCownload} color="success">
-					Descargar QR
-				</Button>
-			</>
-		);
+			link.href = dataURL;
+			link.download = fileName + ".png";
+			link.click();
+		}
 	};
 
 	return (
-		<div className="flex justify-center">
-			<div className="flex max-w-md flex-col items-center gap-3 p-2">
+		<div className="my-4 flex justify-center">
+			<div className="flex max-w-md flex-col items-center gap-3">
 				<Typography color={themeTatailwind.primary.color} variant="h3">
-					Código QR
+					<div className="font-semibold">Código QR</div>
 				</Typography>
 				<Typography color={themeTatailwind.primary.color} variant="body1">
 					Recuerda
 					<span style={{ color: "red" }}> descargar el código QR </span>
 					para poder retirar tu producto de manera presencial.
 				</Typography>
-				{id ? renderQr() : <Typography>Id no encontrado</Typography>}
+				{id ? (
+					<>
+						<div ref={qrRef}>
+							<QRCodeCanvas
+								style={{
+									width: "100%",
+									maxWidth: "100%",
+									height: "100%",
+									padding: "10px",
+								}}
+								value={id}
+								size={512}
+								bgColor={"#ffffff"}
+								fgColor={"#000000"}
+								level={"M"}
+								marginSize={1}
+								minVersion={4}
+							/>
+						</div>
+						<Button
+							variant="contained"
+							onClick={() => downloadQRCode(`qrcode-${id}`)}
+							color="success"
+							endIcon={<CloudDownloadIcon />}
+						>
+							Descargar QR
+						</Button>
+					</>
+				) : (
+					<Typography>Id no encontrado</Typography>
+				)}
 			</div>
 		</div>
 	);
